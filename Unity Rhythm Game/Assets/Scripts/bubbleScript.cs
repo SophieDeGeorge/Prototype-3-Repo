e7a7fcbb6ca8708eefc4c026d8f91db364cc72ae
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-11-06 15:03:41 -0800

    Swapped sphere colliders for circles

--- Unity Rhythm Game/Assets/Scripts/bubbleScript.cs
+++ Unity Rhythm Game/Assets/Scripts/bubbleScript.cs
@@ -7,26 +7,50 @@ public class BubbleScript : MonoBehaviour
 {
     [SerializeField] private CircleCollider2D bubble_cldr;
     [SerializeField] private GameObject badZone;            // set all zones in inspector
-    private SphereCollider bad_cldr;                            // set colliders in start()
+    private CircleCollider2D bad_cldr;                      // set colliders in start()
     [SerializeField] private GameObject goodZone;
-    private SphereCollider good_cldr;
+    private CircleCollider2D good_cldr;
     [SerializeField] private GameObject perfectZone;
-    private SphereCollider perfect_cldr;
+    private CircleCollider2D perfect_cldr;
+    [SerializeField] private GameObject outline;
+    private CircleCollider2D outline_cldr;
 
     public Transform tf;
     private Vector3 incSpeed = new Vector3(0.1f, 0.1f, 0f);
-    [SerializeField] private float incSpeedRadius;
-    private float maxRadius = 4.3f;
+    private string noteZone;
+
 
     void Start()
     {
-        //my_collider = GetComponent<SphereCollider>();
-        bad_cldr = badZone.GetComponent<SphereCollider>();
-        good_cldr = goodZone.GetComponent<SphereCollider>();
-        perfect_cldr = perfectZone.GetComponent<SphereCollider>();
+        //bubble_cldr = GetComponent<CircleCollider2D>(); 
+        bad_cldr = badZone.GetComponent<CircleCollider2D>();
+        good_cldr = goodZone.GetComponent<CircleCollider2D>();
+        perfect_cldr = perfectZone.GetComponent<CircleCollider2D>();
+        outline_cldr = outline.GetComponent<CircleCollider2D>();
+        noteZone = "miss";
     }
 
-    //bubble_cldr.OnCollisionEnter()
+/*
+    private void OnTriggerEnter(CircleCollider2D bubble_cldr) 
+    {
+        if ()
+        {                                                      // if collided with bad_cldr
+            noteZone = "bad";
+        }
+        if ()
+        {                                                       //good_cldr
+            noteZone = "good";
+        }
+        if ()
+        {                                                       //perfect_cldr
+            noteZone = "perfect";
+        }
+        if ()
+        {                                                       //outline cldr
+            noteZone = "miss";
+        }
+    }
+    */
 
     // Update is called once per frame
     void Update()
